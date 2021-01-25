8b69300063cb8610ec22fb2b84fa5517690b1a98
amoral97 <70278391+amoral97@users.noreply.github.com>
2021-01-24 23:11:54 -0700

    Added fxml files

--- Othello/src/Main.java
+++ Othello/src/Main.java
@@ -1,9 +1,23 @@
+import java.io.IOException;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends Application {
 
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene1 = new Scene(root);
+
+		primaryStage.setScene(scene1);
+		primaryStage.show();
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
