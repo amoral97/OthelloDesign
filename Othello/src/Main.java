7b76af5e0b7051ec96b6abe1d3e88f9383a9665c
amoral97 <70278391+amoral97@users.noreply.github.com>
2021-01-25 16:15:26 -0700

    Updated FXML Files

--- Othello/src/Main.java
+++ Othello/src/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 		primaryStage.setScene(scene1);
 		primaryStage.show();
 	}
+	
 
 	public static void main(String[] args) {
 		launch(args);
---
+++ Othello/src/TitlePageController.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class TitlePageController {
+	@FXML
+	public Button button1;
+
+	public void changeMainScene(ActionEvent event) throws IOException {
+		Parent sceneParent = FXMLLoader.load(getClass().getResource("Game.fxml"));
+		Scene scene1 = new Scene(sceneParent);
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		window.setScene(scene1);
+		window.show();
+	}
+}
