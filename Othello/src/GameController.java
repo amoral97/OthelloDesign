f948f7c3bd6a2737bc7204ebc5c1d2f1bac485b6
amoral97 <70278391+amoral97@users.noreply.github.com>
2021-01-26 23:45:21 -0700

    Added GameController class to handle board and disk operations.

---
+++ Othello/src/GameController.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+public class GameController {
+	// @FXML allows scene builder to recognize indicated variables and methods.
+	@FXML
+	Pane pane;
+	
+	private int size = 400;
+	private int tiles = 8;
+	private int rectangleSize = size / tiles;
+	
+	private ArrayList<Disk> whiteDisks;
+	private ArrayList<Disk> blackDisks;
+	
+	@FXML
+	public void initialize()	{
+		// Draws a green gridpane on the javafx pane by getting the size, amount of tiles, and the dividend of the two to draw the individual tiles.
+		for (int i = 0; i < size; i += rectangleSize) {
+			for (int j = 0; j < size; j += rectangleSize) {
+				Rectangle r = new Rectangle(i, j, rectangleSize, rectangleSize);
+				
+				r.setFill(Color.LIMEGREEN);
+				r.setStroke(Color.BLACK);
+				pane.getChildren().add(r);
+			}
+		}
+		// Creates and sets white disks on specific x y coordinates
+		whiteDisks = new ArrayList<Disk>();
+		for (int i = 0; i < tiles; i++) {
+			Circle c = new Circle();
+			c.setFill(Color.WHITE);
+			c.setStroke(Color.BLACK);
+			double radius = rectangleSize / 3.0;
+			int x = 175;
+			int y = 175;
+			
+			Disk d = new Disk(x, y, radius, c);
+			
+			whiteDisks.add(d);
+			pane.getChildren().add(c);
+			d.draw();
+		}
+		// Creates and sets black disks on specific x y coordinates
+		blackDisks = new ArrayList<Disk>();
+		for (int i = 0; i < tiles; i ++) {
+			Circle c = new Circle ();
+			c.setFill(Color.BLACK);
+			c.setStroke(Color.BLACK);
+			double radius = rectangleSize / 3.0;
+			int x = 225;
+			int y = 175;
+			
+			Disk d = new Disk(x, y, radius, c);
+			
+			blackDisks.add(d);
+			pane.getChildren().add(c);
+			d.draw();
+		}
+		
+	}
+}
