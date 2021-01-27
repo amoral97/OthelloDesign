efc43b2bed91917b4c5cf8ec490f466411e49e3b
harrisonGardner <harrisongardner531@gmail.com>
2021-01-27 14:11:04 -0700

    New Function to place disks and mouse click to place disk
    Left original code in incase I am not understanding parts of it and shouldnt have changed it. New function just takes a color and board cords to place the disk. Mouse event checks for clicks on the board and converts the coordinates to board coordinates then places a disk there. Not made to interact with anything yet.

--- Othello/src/GameController.java
+++ Othello/src/GameController.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -30,38 +32,81 @@ public class GameController {
 				pane.getChildren().add(r);
 			}
 		}
+		
+		//Changed to have an easy method to call instead: createDisk(). Left commented out here in case I'm misunderstanding something about it.
 		// Creates and sets white disks on specific x y coordinates
 		whiteDisks = new ArrayList<Disk>();
-		for (int i = 0; i < tiles; i++) {
-			Circle c = new Circle();
-			c.setFill(Color.WHITE);
-			c.setStroke(Color.BLACK);
-			double radius = rectangleSize / 3.0;
-			int x = 175;
-			int y = 175;
-			
-			Disk d = new Disk(x, y, radius, c);
-			
-			whiteDisks.add(d);
-			pane.getChildren().add(c);
-			d.draw();
-		}
+//		for (int i = 0; i < tiles; i++) {
+//			Circle c = new Circle();
+//			c.setFill(Color.WHITE);
+//			c.setStroke(Color.BLACK);
+//			double radius = rectangleSize / 3.0;
+//			int x = 3 * rectangleSize + (rectangleSize/2);
+//			int y = 3 * rectangleSize + (rectangleSize/2);
+//			
+//			Disk d = new Disk(x, y, radius, c);
+//			
+//			whiteDisks.add(d);
+//			pane.getChildren().add(c);
+//			d.draw();
+//		}
 		// Creates and sets black disks on specific x y coordinates
 		blackDisks = new ArrayList<Disk>();
-		for (int i = 0; i < tiles; i ++) {
-			Circle c = new Circle ();
-			c.setFill(Color.BLACK);
-			c.setStroke(Color.BLACK);
-			double radius = rectangleSize / 3.0;
-			int x = 225;
-			int y = 175;
-			
-			Disk d = new Disk(x, y, radius, c);
-			
-			blackDisks.add(d);
-			pane.getChildren().add(c);
-			d.draw();
-		}
+//		for (int i = 0; i < tiles; i ++) {
+//			Circle c = new Circle ();
+//			c.setFill(Color.BLACK);
+//			c.setStroke(Color.BLACK);
+//			double radius = rectangleSize / 3.0;
+//			int x = 4 * rectangleSize + (rectangleSize/2);
+//			int y = 3 * rectangleSize + (rectangleSize/2);
+//			
+//			Disk d = new Disk(x, y, radius, c);
+//			
+//			blackDisks.add(d);
+//			pane.getChildren().add(c);
+//			d.draw();
+//		}
 		
+		//Initial board disks
+		createDisk(Color.BLACK, 4, 3);
+		createDisk(Color.BLACK, 3, 4);
+		createDisk(Color.WHITE, 3, 3);
+		createDisk(Color.WHITE, 4, 4);
+		
+		mouseClickChecker();
+		
+	}
+	
+	//Create a disk based on color and board coordinates
+	public void createDisk(Color color, int column, int row)
+	{
+		Circle c = new Circle ();
+		c.setFill(color);
+		c.setStroke(Color.BLACK);
+		double radius = rectangleSize / 3.0;
+		int x = column * rectangleSize + (rectangleSize/2);
+		int y = row * rectangleSize + (rectangleSize/2);
+		
+		Disk d = new Disk(x, y, radius, c);
+		
+		if(color == Color.WHITE) whiteDisks.add(d);
+		if(color == Color.BLACK) blackDisks.add(d);
+		pane.getChildren().add(c);
+		d.draw();
+	}
+	
+	//Temporary, Seeing how to handle mouse clicks and placing disks on the board.
+	//Works so far but not made to interact with much else
+	public void mouseClickChecker()
+	{
+		pane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		    @Override
+		    public void handle(MouseEvent event) {
+		        System.out.println(event.getSceneX());
+		        System.out.println(event.getSceneY());
+		        
+		        createDisk(Color.BLACK, (int)event.getSceneX()/rectangleSize, (int)event.getSceneY()/rectangleSize);
+		    }
+		});
 	}
 }
