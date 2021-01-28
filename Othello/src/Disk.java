df748b6ab21e947ebfd44bc18a858e0c57ef1170
harrisonGardner <harrisongardner531@gmail.com>
2021-01-28 00:02:07 -0700

    Added some move checks
    The adjacent piece check looks pretty gross but it works. I changed over to an int array because I couldn't see how we would handle the line checks with objects.
    
    Right now the array just has 0 for no disk, 1 for a white disk, 2 for a black disk, and then a -1 for a valid move.
    
    If you try to make an invalid move the array will print itself to the console for debug purposes right now.

--- Othello/src/Disk.java
+++ Othello/src/Disk.java
@@ -1,37 +1,18 @@
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class Disk {
+public class Disk extends GameController{
 
-	private double x;
-	private double y;
+	public int column;
+	public int row;
 	private double radius;
 	private Circle circle;
 
-	public Disk(double x, double y, double radius, Circle c) {
-		this.x = x;
-		this.y = y;
+	public Disk(int column, int row, double radius, Circle c) {
+		this.column = column;
+		this.row = row;
 		this.radius = radius;
 		this.circle = c;
-
-	}
-
-	public void setY(double y) {
-		this.y = y;
-	}
-
-	public double getY() {
-		return y;
-
-	}
-
-	public void setX(double x) {
-		this.x = x;
-
-	}
-
-	public double getX() {
-		return x;
 	}
 
 	public void setColor(Color color) {
@@ -40,7 +21,17 @@ public class Disk {
 
 	public void draw() {
 		circle.setRadius(radius);
-		circle.setTranslateX(x);
-		circle.setTranslateY(y);
+		circle.setTranslateX(column * rectangleSize + (rectangleSize/2));
+		circle.setTranslateY(row * rectangleSize + (rectangleSize/2));
 	}
+
+	public int getColumn() {
+		return this.column;
+	}
+
+	public int getRow() {
+		return this.row;
+	}
+	
+	
 }
--- Othello/src/GameController.java
+++ Othello/src/GameController.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -13,10 +15,10 @@ public class GameController {
 	
 	private int size = 400;
 	private int tiles = 8;
-	private int rectangleSize = size / tiles;
+	public int rectangleSize = size / tiles;
 	
-	private ArrayList<Disk> whiteDisks;
-	private ArrayList<Disk> blackDisks;
+	private ArrayList<Disk> diskArray;
+	private int[][] board;
 	
 	@FXML
 	public void initialize()	{
@@ -30,38 +32,170 @@ public class GameController {
 				pane.getChildren().add(r);
 			}
 		}
-		// Creates and sets white disks on specific x y coordinates
-		whiteDisks = new ArrayList<Disk>();
-		for (int i = 0; i < tiles; i++) {
-			Circle c = new Circle();
-			c.setFill(Color.WHITE);
-			c.setStroke(Color.BLACK);
-			double radius = rectangleSize / 3.0;
-			int x = 3 * rectangleSize + (rectangleSize/2);
-			int y = 3 * rectangleSize + (rectangleSize/2);
-			
-			Disk d = new Disk(x, y, radius, c);
-			
-			whiteDisks.add(d);
-			pane.getChildren().add(c);
-			d.draw();
-		}
-		// Creates and sets black disks on specific x y coordinates
-		blackDisks = new ArrayList<Disk>();
-		for (int i = 0; i < tiles; i ++) {
-			Circle c = new Circle ();
-			c.setFill(Color.BLACK);
-			c.setStroke(Color.BLACK);
-			double radius = rectangleSize / 3.0;
-			int x = 4 * rectangleSize + (rectangleSize/2);
-			int y = 3 * rectangleSize + (rectangleSize/2);
-			
-			Disk d = new Disk(x, y, radius, c);
-			
-			blackDisks.add(d);
-			pane.getChildren().add(c);
-			d.draw();
+		
+
+		//Disk Object Array initialize
+		diskArray = new ArrayList<Disk>();
+		
+		//Board Array Initialize
+		board = new int[tiles][tiles];
+		for(int i = 0; i < tiles; i++)
+		{
+			for(int z = 0; z < tiles; z++)
+			{
+				board[z][i] = 0;
+			}
 		}
 		
+		//Initial board disks
+		createDisk(Color.BLACK, 4, 3);
+		createDisk(Color.BLACK, 3, 4);
+		createDisk(Color.WHITE, 3, 3);
+		createDisk(Color.WHITE, 4, 4);
+		
+		mouseClickChecker();
+		
+	}
+	
+	//Create a disk based on color and board coordinates
+	private void createDisk(Color color, int column, int row)
+	{
+		Circle c = new Circle ();
+		c.setFill(color);
+		c.setStroke(Color.BLACK);
+		double radius = rectangleSize / 3.0;
+		
+		Disk d = new Disk(column, row, radius, c);
+		
+		diskArray.add(d);
+		
+		//Board Array Key: 0 = blank, 1 = white, 2 = black
+		if (color == Color.WHITE) board[column][row] = 1;
+		if (color == Color.BLACK) board[column][row] = 2;
+		
+		System.out.println(row);
+		
+		pane.getChildren().add(c);
+		d.draw();
+	}
+	
+	
+	
+	//Temporary, Seeing how to handle mouse clicks and placing disks on the board.
+	//Works so far but not made to interact with much else
+	private void mouseClickChecker()
+	{
+		pane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+		    public void handle(MouseEvent event) {
+		    	if(validMoveCheck((int)event.getSceneX()/rectangleSize, (int)event.getSceneY()/rectangleSize)) createDisk(Color.BLACK, (int)event.getSceneX()/rectangleSize, (int)event.getSceneY()/rectangleSize);
+		        else {System.out.println("Invalid Move"); debugDisplayBoard();}
+		    }
+		});
+	}
+	
+	private boolean validMoveCheck(int column, int row)
+	{
+		boolean validMove = true;
+		
+		//Decide valid disk spots
+		//This is gonna look really gross
+		for(int i = 0; i < tiles; i++)
+		{
+			for(int z = 0; z < tiles; z++)
+			{
+				if(board[z][i] == 0)
+				{
+					if(i == 0 && z == 0)
+					{
+						if(board[z+1][i] > 0)board[z][i] = -1;
+						if(board[z+1][i+1] > 0)board[z][i] = -1;
+						if(board[z][i+1] > 0)board[z][i] = -1;
+					}
+					else if(i == 0 && z < 7)
+					{
+						if(board[z+1][i] > 0)board[z][i] = -1;
+						if(board[z-1][i] > 0)board[z][i] = -1;
+						if(board[z][i+1] > 0)board[z][i] = -1;
+						if(board[z+1][i+1] > 0)board[z][i] = -1;
+						if(board[z-1][i+1] > 0)board[z][i] = -1;
+					}
+					else if(i == 0 && z == 7)
+					{
+						if(board[z-1][i] > 0)board[z][i] = -1;
+						if(board[z][i+1] > 0)board[z][i] = -1;
+						if(board[z-1][i+1] > 0)board[z][i] = -1;
+					}
+					else if(i < 7 && z == 7)
+					{
+						if(board[z-1][i] > 0)board[z][i] = -1;
+						if(board[z][i+1] > 0)board[z][i] = -1;
+						if(board[z][i-1] > 0)board[z][i] = -1;
+						if(board[z-1][i-1] > 0)board[z][i] = -1;
+						if(board[z-1][i+1] > 0)board[z][i] = -1;
+					}
+					else if(i == 7 && z == 0)	
+					{
+						if(board[z+1][i] > 0)board[z][i] = -1;
+						if(board[z][i-1] > 0)board[z][i] = -1;
+						if(board[z+1][i-1] > 0)board[z][i] = -1;
+					}
+					else if(i < 7 && z == 0)
+					{
+						if(board[z+1][i] > 0)board[z][i] = -1;
+						if(board[z][i+1] > 0)board[z][i] = -1;
+						if(board[z][i-1] > 0)board[z][i] = -1;
+						if(board[z+1][i+1] > 0)board[z][i] = -1;
+						if(board[z+1][i-1] > 0)board[z][i] = -1;
+					}
+					else if(i == 7 && z == 7)
+					{
+						if(board[z-1][i] > 0)board[z][i] = -1;
+						if(board[z][i-1] > 0)board[z][i] = -1;
+						if(board[z-1][i-1] > 0)board[z][i] = -1;
+					}
+					else if(i == 7 && z < 7)
+					{
+						if(board[z+1][i] > 0)board[z][i] = -1;
+						if(board[z-1][i] > 0)board[z][i] = -1;
+						if(board[z][i-1] > 0)board[z][i] = -1;
+						if(board[z-1][i-1] > 0)board[z][i] = -1;
+						if(board[z+1][i-1] > 0)board[z][i] = -1;
+					}
+					else if(i < 7 && z < 7)
+					{
+						if(board[z+1][i] > 0)board[z][i] = -1;
+						if(board[z-1][i] > 0)board[z][i] = -1;
+						if(board[z][i+1] > 0)board[z][i] = -1;
+						if(board[z][i-1] > 0)board[z][i] = -1;
+						if(board[z+1][i+1] > 0)board[z][i] = -1;
+						if(board[z-1][i-1] > 0)board[z][i] = -1;
+						if(board[z+1][i-1] > 0)board[z][i] = -1;
+						if(board[z-1][i+1] > 0)board[z][i] = -1;
+					}
+				}
+			}
+		}
+
+		//Check if a disk is already there
+        if(board[column][row] != -1) validMove = false;
+        
+		return validMove;
+	}
+	
+	public void debugDisplayBoard()
+	{
+		for(int i = 0; i < tiles; i++)
+		{
+			for(int z = 0; z < tiles; z++)
+			{
+				System.out.print(board[z][i]);
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public int getRectangleSize() {
+		return rectangleSize;
 	}
 }
